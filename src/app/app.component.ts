eda347259e841d6de0c0342534170b75039f7186
sami-mai <samirah.maison@gmail>
2018-03-20 12:37:20 +0300

    create new goal component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+// import Goal class here
+import { Goal } from './goal';
+
 
 @Component({
   selector: 'app-root',
@@ -7,12 +10,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Goals2';
-  goal = "Watching Finding Nemo";
-  goals:string[];
+  // goal = "Watching Finding Nemo";
+  // goals:string[];
+
+// Change array of Goals
+  goals = [
+        new Goal(1,'Watch Finding Nemo'),
+        new Goal(2,'Buy Cookies'),
+        new Goal(3, 'Get new Phone Case'),
+        new Goal(4, 'Get Dog Food'),
+        new Goal(5, 'Solve math homework'),
+        new Goal(6, 'Plot my world domination plan'),
+
+    ]
 
   constructor(){
 
-    this.goals = ["Watching Black Panther", "Buy fruits", "Get a new heater"];
-    
+    // this.goals = ["Watching Black Panther", "Buy fruits", "Get a new heater"];
+
   }
 }
