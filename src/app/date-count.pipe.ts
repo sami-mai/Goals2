752df3b8fbd72cb9789526a2d4d1dafa112e19e5
sami-mai <samirah.maison@gmail>
2018-03-21 09:44:01 +0300

    Add logic to count dates

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -4,9 +4,22 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'dateCount'
 })
 export class DateCountPipe implements PipeTransform {
+// Add logic to count dates
+  transform(value: any): number {
+    let today:Date = new Date(); //get current date and time
+    let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
+    var dateDifference =Math.abs(value-todayWithNoTime )// returns value in milliseconds
+    const secondsInADay= 86400; //60 seconds*60 minutes in an hour *24 hours
 
-  transform(value: any, args?: any): any {
-    return null;
+    var dateDifferenceSeconds=dateDifference*0.001; //converts to seconds
+
+    var dateCounter = dateDifferenceSeconds/secondsInADay;
+
+    if (dateCounter >= 1){
+        return dateCounter;
+    }else{
+        return 0;
+    }
   }
 
 }
