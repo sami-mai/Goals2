4c5ef87a715cfe82202bc60ae0eda693e37bd334
sami-mai <samirah.maison@gmail>
2018-03-21 11:11:36 +0300

    import the goal class and create a newGoal property in our GoalFormComponentclass

--- src/app/goal-form/goal-form.component.ts
+++ src/app/goal-form/goal-form.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+// import the goal class
+import {Goal} from '../goal';
 @Component({
   selector: 'app-goal-form',
   templateUrl: './goal-form.component.html',
   styleUrls: ['./goal-form.component.css']
 })
 export class GoalFormComponent implements OnInit {
+// create a newGoal property in our GoalFormComponentclass
+  newGoal=new Goal(0,"","",new Date());
 
   constructor() { }
 
